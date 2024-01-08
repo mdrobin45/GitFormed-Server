d0cfa9914935601b6d2c8ec619dd14745da3d082
Md Robin Rana <developer.smrobin45@gmail.com>
2024-01-08 15:07:20 +0600

    modify sort repo condition

--- src/controllers/repo/sortRepo.js
+++ src/controllers/repo/sortRepo.js
@@ -1,30 +1,42 @@
-const { RepoModel } = require("../../models/models");
+const { RepoModel, UserModel } = require("../../models/models");
 
 const sortRepo = async (req, res) => {
    try {
-      let repo = req.query.repo || "all";
+      let email = req.query.repo || "all";
       const sortBy = req.query.sortBy || "latest";
       const myWatching = req.query.myWatching || false;
 
       // Query repositories
-      query = repo === "all" ? {} : { repoUserEmail: repo };
+      query = email === "all" ? {} : { repoUserEmail: email };
 
-      let response = await RepoModel.find(query);
+      // Find user id with email
+      const user = await UserModel.findOne({ email });
 
-      // Sort repositories
-      if (sortBy === "latest") {
-         response = response.sort((a, b) => a.createdAt - b.createdAt);
-      } else if (sortBy === "alphabetical") {
-         response = response.sort((a, b) =>
-            a.repoName.toLowerCase().localeCompare(b.repoName.toLowerCase())
-         );
-      } else if (sortBy === "watchers") {
-         response = response.sort(
-            (a, b) => b.repoWatchers.length - a.repoWatchers.length
-         );
+      if (myWatching === "true") {
+         console.log("querying watch");
+         const watchingRepo = await RepoModel.find({
+            repoWatchers: { $in: [user?._id] },
+         });
+         return res.status(200).json(watchingRepo);
+      } else {
+         console.log("querying filter");
+         let response = await RepoModel.find(query);
+
+         // Sort repositories
+         if (sortBy === "latest") {
+            response = response.sort((a, b) => a.createdAt - b.createdAt);
+         } else if (sortBy === "alphabetical") {
+            response = response.sort((a, b) =>
+               a.repoName.toLowerCase().localeCompare(b.repoName.toLowerCase())
+            );
+         } else if (sortBy === "watchers") {
+            response = response.sort(
+               (a, b) => b.repoWatchers.length - a.repoWatchers.length
+            );
+         }
+
+         return res.status(200).json(response);
       }
-
-      res.status(200).json(response);
    } catch (err) {
       console.log(err);
       res.status(500).json({ error: "Internal server error!" });
