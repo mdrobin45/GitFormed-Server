5664e1768c4ccccf629a58e9ea1b83c145412f72
Md Robin Rana <developer.smrobin45@gmail.com>
2024-01-11 11:30:22 +0600

    uninstall socket

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 const express = require("express");
-const { createServer } = require("http");
-const { Server } = require("socket.io");
 const cors = require("cors");
 require("dotenv").config();
 const database = require("./src/database");
 const repoRouter = require("./src/routes/repositories");
 const pullReqRouter = require("./src/routes/pullrequest");
 const userRouter = require("./src/routes/user");
-const socketConnection = require("./src/utils/socketConnection");
 
 const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-   cors: {
-      origin: process.env.LOCAL_SITE,
-   },
-});
 
 const port = process.env.PORT || 3000;
 
@@ -31,12 +22,6 @@ app.use(express.json());
 // Database connection
 database();
 
-// Middleware for socket io to access from anywhere
-app.use((req, res, next) => {
-   req.io = io;
-   next();
-});
-
 // application routes
 app.get("/", (req, res) => {
    res.send("Server running");
@@ -45,10 +30,7 @@ app.use("/repositories", repoRouter);
 app.use("/pull-request", pullReqRouter);
 app.use("/user", userRouter);
 
-// Socket connection
-socketConnection(io);
-
 // Listen server
-server.listen(port, () => {
+app.listen(port, () => {
    console.log(`Server is running in ${port}`);
 });
